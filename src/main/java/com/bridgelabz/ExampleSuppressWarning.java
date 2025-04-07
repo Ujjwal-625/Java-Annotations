ea33af9a836223b00eb666ddbc15437600c3093b
Ujjwal <Ujjwalrawat248@gmail.com>
2025-04-07 10:35:31 +0530

    [Author:Ujjwal] ADD:ExampleSupressWarning.java

---
+++ src/main/java/com/bridgelabz/ExampleSuppressWarning.java
@@ -0,0 +1,10 @@
+package com.bridgelabz;
+
+import java.util.ArrayList;
+
+public class ExampleSuppressWarning {
+    @SuppressWarnings("unchecked")
+    public static void main(String[] args) {
+        ArrayList list = new ArrayList();
+    }
+}
